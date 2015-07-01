199afd55ea535c143b8991292c9650f1580efdfb
Dromero <diejavrom79@gmail.com>
2015-07-01 18:07:10 -0300

    Faltaba contemplar los remitos que no tenían factura asociada. De paso, agrego el seteo de nro de sucursal.

--- BackGestionTextilLevel/sql/upgrades/produccion/2015-06-28_prueba_fact_electronica.sql
+++ BackGestionTextilLevel/sql/upgrades/produccion/2015-06-28_prueba_fact_electronica.sql
@@ -5,7 +5,9 @@ set f.a_nro_factura =f.a_nro_factura*(-1),
 
 update t_factura f set f.a_nro_factura = f.a_nro_factura*(-1) where f.a_nro_factura > 0;
 
-update T_PARAM_GENERALES set A_NRO_COMIENZO_FACTURA = 0;
+update T_PARAM_GENERALES set A_NRO_COMIENZO_FACTURA = 0, A_NRO_SUCURSAL=2;
+
+update t_remito r set a_nro_factura=(-1)*a_nro_factura where a_nro_factura > 0;
 
 ALTER TABLE `gtl`.`t_factura` DROP INDEX `IX_UNIQUE_NRO_FC`;
 ALTER TABLE `gtl`.`t_factura` ADD UNIQUE INDEX `IX_UNIQUE_NRO_FC`(`A_ID_TIPO_FACTURA`, `TIPO`, `A_NRO_FACTURA`);
