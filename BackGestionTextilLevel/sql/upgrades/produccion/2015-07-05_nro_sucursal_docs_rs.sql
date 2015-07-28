d80a110fd727bb2b6941ce3ffccb91d4ab24901e
Dromero <diejavrom79@gmail.com>
2015-07-28 18:37:28 -0300

    ya se ejecutó en PROD (A y B)

--- BackGestionTextilLevel/sql/upgrades/produccion/2015-07-05_nro_sucursal_docs_rs.sql
+++ BackGestionTextilLevel/sql/upgrades/produccion/2015-07-05_nro_sucursal_docs_rs.sql
@@ -1,3 +1,5 @@
+-- PARA LA A
+
 update t_factura set a_nro_sucursal = 1 where a_nro_factura < 0;
 update t_factura set a_nro_sucursal = 2 where a_nro_factura > 0;
 
@@ -15,3 +17,9 @@ where f.a_nro_factura < 0 and f.a_nro_sucursal = 1 and f.a_id_tipo_factura = 1;
 
 ALTER TABLE `gtl`.`t_factura` DROP INDEX `IX_UNIQUE_NRO_FC`;
 ALTER TABLE `gtl`.`t_factura` ADD UNIQUE INDEX `IX_UNIQUE_NRO_FC`(`A_ID_TIPO_FACTURA`, `TIPO`, `A_NRO_FACTURA`, `A_NRO_SUCURSAL`);
+
+--PARA LA B
+update t_factura set a_nro_sucursal = 1;
+update t_remito r set a_nro_sucursal = 1 where a_nro_factura is not null;
+
+
