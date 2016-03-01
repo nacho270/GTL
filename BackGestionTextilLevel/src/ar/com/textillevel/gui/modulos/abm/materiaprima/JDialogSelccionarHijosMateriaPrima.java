8f5609e261d6432f9c51039642cb21ac2e416984
Ignacio Cicero <ignaciocicero@gmail.com>
2016-03-01 15:24:58 -0300

    Buscador en materias primas hijas #40

--- BackGestionTextilLevel/src/ar/com/textillevel/gui/modulos/abm/materiaprima/JDialogSelccionarHijosMateriaPrima.java
+++ BackGestionTextilLevel/src/ar/com/textillevel/gui/modulos/abm/materiaprima/JDialogSelccionarHijosMateriaPrima.java
@@ -6,6 +6,8 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -21,7 +24,9 @@ import org.apache.commons.collections.Predicate;
 
 import ar.com.fwcommon.componentes.FWCheckBoxList;
 import ar.com.fwcommon.componentes.FWJOptionPane;
+import ar.com.fwcommon.componentes.FWJTextField;
 import ar.com.fwcommon.util.GuiUtil;
+import ar.com.fwcommon.util.StringUtil;
 import ar.com.textillevel.entidades.ventas.materiaprima.MateriaPrima;
 import ar.com.textillevel.facade.api.remote.MateriaPrimaFacadeRemote;
 import ar.com.textillevel.util.GTLBeanFactory;
@@ -30,6 +35,7 @@ public class JDialogSelccionarHijosMateriaPrima extends JDialog{
 
 	private static final long serialVersionUID = -1182859205318727998L;
 	
+	private FWJTextField txtBusqueda;
 	private JButton btnCancelar;
 	private JButton btnAceptar;
 	private FWCheckBoxList<MateriaPrima> listaMps;
@@ -37,12 +43,16 @@ public class JDialogSelccionarHijosMateriaPrima extends JDialog{
 	private boolean acepto;
 	private MateriaPrima materiaPrima;
 	
+	private List<MateriaPrima> mps;
 	private List<MateriaPrima> materiasPrimasSeleccionadas;
 	
 	public JDialogSelccionarHijosMateriaPrima(Frame padre, MateriaPrima materiaPrimaActual) {
 		super(padre);
 		this.materiaPrima = materiaPrimaActual;
 		this.materiasPrimasSeleccionadas = new ArrayList<MateriaPrima>(materiaPrimaActual.getMpHijas());
+		boolean yaTieneHijas = !materiaPrima.getMpHijas().isEmpty();
+		this.mps = GTLBeanFactory.getInstance().getBean2(MateriaPrimaFacadeRemote.class).
+				getAllOrderByTipos(yaTieneHijas, materiaPrima.getTipo());
 		setUpComponentes();
 		setUpScreen();
 	}
@@ -57,18 +67,23 @@ public class JDialogSelccionarHijosMateriaPrima extends JDialog{
 			}
 		});
 		setModal(true);
-		setSize(new Dimension(250, 400));
+		setSize(new Dimension(350, 430));
 		setResizable(false);
 		GuiUtil.centrar(this);
 	}
 
 	private void setUpComponentes() {
+		JPanel panelNorte = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panelNorte.add(new JLabel("Buscar: "));
+		panelNorte.add(getTxtBusqueda());
+		add(panelNorte, BorderLayout.NORTH);
+
 		add(new JScrollPane(getListaMps(), JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
 
 		JPanel panelSur = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		panelSur.add(getBtnAceptar());
 		panelSur.add(getBtnCancelar());
-		add(panelSur, BorderLayout.SOUTH);		
+		add(panelSur, BorderLayout.SOUTH);
 	}
 
 	public FWCheckBoxList<MateriaPrima> getListaMps() {
@@ -87,31 +102,31 @@ public class JDialogSelccionarHijosMateriaPrima extends JDialog{
 					}
 				}
 			};
-			boolean yaTieneHijas = !materiaPrima.getMpHijas().isEmpty();
-			List<MateriaPrima> mps = GTLBeanFactory.getInstance().getBean2(MateriaPrimaFacadeRemote.class).
-					getAllOrderByTipos(yaTieneHijas, materiaPrima.getTipo());
-			if(!yaTieneHijas) {
-				CollectionUtils.filter(mps, new Predicate() {
-					public boolean evaluate(Object arg0) {
-						MateriaPrima mp = (MateriaPrima) arg0;
-						return mp.getMpHijas().isEmpty();
-					}
-				});
-			}
-			
-			Object[] materiasPrimas = new Object[mps.size() - 1]; // para no incluir el actual
-			int i=0;
-			for(MateriaPrima m : mps) {
-				if(m.getId().intValue() != materiaPrima.getId().intValue()) {
-					materiasPrimas[i++] = m;
-				}
-			}
-			getListaMps().setValues(materiasPrimas);
-			for(MateriaPrima mp : materiaPrima.getMpHijas()) {
-				getListaMps().setSelectedValue(mp, false);
-			}
+			llenarListaCheckboxes(null);
 		}
 		return listaMps;
+	}
+
+	private void llenarListaCheckboxes(final String filter) {
+		if (getListaMps().getValueIsAdjusting()) return;
+		boolean yaTieneHijas = !materiaPrima.getMpHijas().isEmpty();
+		List<MateriaPrima> mps2 = new ArrayList<MateriaPrima>(this.mps);
+		if(!yaTieneHijas) {
+			CollectionUtils.filter(mps2, new Predicate() {
+				public boolean evaluate(Object arg0) {
+					MateriaPrima mp = (MateriaPrima) arg0;
+					boolean noEsLaQueEdito = mp.getId().intValue() != materiaPrima.getId().intValue();
+					if (StringUtil.isNullOrEmpty(filter)) {
+						return noEsLaQueEdito && mp.getMpHijas().isEmpty();
+					}
+					return noEsLaQueEdito && mp.getDescripcion().toUpperCase().contains(filter.toUpperCase());
+				}
+			});
+		}
+		getListaMps().setValues(mps2.toArray());
+		for(MateriaPrima mp : materiaPrima.getMpHijas()) {
+			getListaMps().setSelectedValue(mp, false);
+		}
 	}
 
 	public boolean isAcepto() {
@@ -156,5 +171,19 @@ public class JDialogSelccionarHijosMateriaPrima extends JDialog{
 			});
 		}
 		return btnAceptar;
+	}
+
+	public FWJTextField getTxtBusqueda() {
+		if (txtBusqueda == null) {
+			txtBusqueda = new FWJTextField();
+			txtBusqueda.setPreferredSize(new Dimension(170, 20));
+			txtBusqueda.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyReleased(KeyEvent e) {
+					llenarListaCheckboxes(txtBusqueda.getText());
+				}
+			});
+		}
+		return txtBusqueda;
 	}
 }
