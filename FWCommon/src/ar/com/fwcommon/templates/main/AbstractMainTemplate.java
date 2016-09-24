c924fa0353801a29ec3b9c6b0bd934fe6b6d9305
Ignacio Cicero <ignaciocicero@gmail.com>
2016-09-24 10:10:25 -0300

    Soporte para fondo blanco si queremos usar Nimbus LookAndFeel

--- FWCommon/src/ar/com/fwcommon/templates/main/AbstractMainTemplate.java
+++ FWCommon/src/ar/com/fwcommon/templates/main/AbstractMainTemplate.java
@@ -1,9 +1,11 @@
 package ar.com.fwcommon.templates.main;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -12,13 +14,17 @@ import java.util.Collection;
 import java.util.Vector;
 
 import javax.swing.Icon;
+import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.Painter;
 import javax.swing.SwingUtilities;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
 
 import org.jdesktop.jdic.tray.SystemTray;
 import org.jdesktop.jdic.tray.TrayIcon;
@@ -450,6 +456,25 @@ public abstract class AbstractMainTemplate<T extends ILoginManager> extends JFra
 
 	/* Clase para el manejo del desktop pane */
 	protected class FWDesktopPane extends MDIDesktopPane {
+		
+		@Override
+		public void updateUI() {
+			if ("Nimbus".equals(UIManager.getLookAndFeel().getName())) {
+				UIDefaults map = new UIDefaults();
+				Painter<JComponent> painter = new Painter<JComponent>() {
+					
+					@Override
+					public void paint(Graphics2D g, JComponent object, int width, int height) {
+						g.setColor(Color.white);
+						g.fillRect(0, 0, width, height);
+					}
+				};
+				map.put("DesktopPane[Enabled].backgroundPainter", painter);
+				putClientProperty("Nimbus.Overrides", map);
+			}
+			super.updateUI();
+		}
+		
 		/** Cierra todas las ventanas (InternalFrame) abiertas */
 		public boolean cerrarVentanas() {
 			JInternalFrame frames[] = getAllFrames();
