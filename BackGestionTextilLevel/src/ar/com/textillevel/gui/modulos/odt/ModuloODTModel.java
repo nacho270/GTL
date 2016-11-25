b10c17b55a0d13ab2cbea3796c529dce0e1df1a5
Javier Romero <Javier Romero@192.168.0.34>
2016-11-24 23:45:33 -0300

    elimino edu.emory.mathcs.backport.java.util.Collections;

--- BackGestionTextilLevel/src/ar/com/textillevel/gui/modulos/odt/ModuloODTModel.java
+++ BackGestionTextilLevel/src/ar/com/textillevel/gui/modulos/odt/ModuloODTModel.java
@@ -12,7 +12,6 @@ import ar.com.textillevel.gui.acciones.RemitoEntradaBusinessDelegate;
 import ar.com.textillevel.gui.modulos.odt.builder.BuilderAccionesODT;
 import ar.com.textillevel.gui.modulos.odt.cabecera.ModeloCabeceraODT;
 import ar.com.textillevel.modulos.odt.entidades.OrdenDeTrabajo;
-import edu.emory.mathcs.backport.java.util.Collections;
 
 public class ModuloODTModel extends ModuloModel<OrdenDeTrabajo, ModeloCabeceraODT> {
 	
@@ -34,7 +33,6 @@ public class ModuloODTModel extends ModuloModel<OrdenDeTrabajo, ModeloCabeceraOD
 		setTitulo("Administrar Ordenes de trabajo");
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<OrdenDeTrabajo> buscarItems(ModeloCabeceraODT modeloCabecera) {
 		try {
@@ -42,7 +40,7 @@ public class ModuloODTModel extends ModuloModel<OrdenDeTrabajo, ModeloCabeceraOD
 		} catch (RemoteException e) {
 			e.printStackTrace();
 			FWJOptionPane.showErrorMessage(null, StringW.wordWrap(e.getMessage()), "Error");
-			return Collections.emptyList();
+			return java.util.Collections.emptyList();
 		}
 	}
 
