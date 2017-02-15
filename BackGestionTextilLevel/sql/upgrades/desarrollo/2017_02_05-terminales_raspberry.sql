e89889d4e3cd9581e4ef4722538f1de430c8f4aa
Javier Romero <Javier Romero@192.168.0.20>
2017-02-15 16:34:29 -0300

    #96 (había intercambiado contenido)

--- BackGestionTextilLevel/sql/upgrades/desarrollo/2017_02_05-terminales_raspberry.sql
+++ BackGestionTextilLevel/sql/upgrades/desarrollo/2017_02_05-terminales_raspberry.sql
@@ -1,1 +1,16 @@
-INSERT INTO T_MODULO VALUES (115,'main.acciones.pagos.EntregaReingresoDocumentosAction','Pagos - Entrega Reingreso de Documentos',0,0);
+-- INSERT DE LOS MODULOS DISPONIBLES
+INSERT INTO T_MODULO_TERMINAL (P_ID, A_CLASE, a_NOMBRE) VALUES(1, 'main.acciones.VerDialogoOrdenarPiezasODTAction', 'Cosido');
+INSERT INTO T_MODULO_TERMINAL (P_ID, A_CLASE, a_NOMBRE) VALUES(2, 'main.acciones.VerLectorODTAction', 'Asignar metros piezas ODT');
+INSERT INTO T_MODULO_TERMINAL (P_ID, A_CLASE, a_NOMBRE) VALUES(3, 'main.acciones.VerEntregaReingresoDocumentosAction', 'Entrega/Reingreso documentos');
+INSERT INTO T_MODULO_TERMINAL (P_ID, A_CLASE, a_NOMBRE) VALUES(4, 'main.acciones.VerDialogoEntregarMercaderiaRemitoSalidaAction', 'Control de Mercadería');
+
+
+-- INSERT DE LAS TERMINALES
+INSERT INTO T_TERMINAL VALUES(1, 'TERMINAL COSIDO', 'por definir', 1);
+INSERT INTO T_TERMINAL VALUES(2, 'TERMINAL ENTREGA', 'por definir', NULL);
+INSERT INTO T_TERMINAL VALUES(3, 'TERMINAL ASIGNACION METROS', 'por definir', 2);
+INSERT INTO T_TERMINAL VALUES(4, 'TERMINAL CONTROL DE SALIDA', 'por definir', 3);
+
+
+-- INSERT DEL MODULO DE ASIGNACION DE TERMINALES
+INSERT INTO T_MODULO VALUES (114,'ar.com.textillevel.gui.modulos.abm.GuiABMTerminal','Terminal - Administrar módulos de terminales',0,0);
