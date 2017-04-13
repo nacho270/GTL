de17fbf41f9122cd8d087fb5dfd83de9a88bde08
Javier Romero <Javier Romero@192.168.0.24>
2017-04-13 12:27:57 -0300

    ya se ejecutaron en PROD

--- BackGestionTextilLevel/sql/upgrades/produccion/2017_03_27_issue_18_gtllite.sql
+++ BackGestionTextilLevel/sql/upgrades/produccion/2017_03_27_issue_18_gtllite.sql
@@ -4,6 +4,7 @@ update t_modulo_terminal set a_requiere_login=1;
 -- sólo en A
 insert into t_modulo_terminal values (5, 'main.acciones.ProcesarODTEnSectorHumedoAction', 'Sector Teñido', 0);
 insert into t_modulo_terminal values (6, 'main.acciones.ProcesarODTEnSectorSecoAction', 'Sector Seco', 0);
+insert into t_modulo_terminal values (7, 'main.acciones.ProcesarODTEnSectorEstampadoAction', 'Sector Estampería', 0);
 
 -- en A y B
 ALTER TABLE t_transicion_odt MODIFY COLUMN F_USUARIO_P_ID INT(11) DEFAULT NULL;
