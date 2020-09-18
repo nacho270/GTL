7909ed10fb14e24b1536e433546399afb9891467
Javier Romero <diejavrom79@gmail.com>
2020-09-17 21:58:24 -0300

    #175 fix en el agregado de subpiezas en RS

--- BackGestionTextilLevel/src/ar/com/textillevel/gui/acciones/remitosalida/JDialogAgregarRemitoSalida.java
+++ BackGestionTextilLevel/src/ar/com/textillevel/gui/acciones/remitosalida/JDialogAgregarRemitoSalida.java
@@ -623,11 +623,10 @@ public class JDialogAgregarRemitoSalida extends JDialog {
 		public PanelTablaPieza(RemitoSalida remitoSalida, boolean modoConsulta) {
 			setModoConsulta(modoConsulta);
 			initializePopupMenu();
-			//no agrego los botones de combinar piezas porque la idea es que eso se haga desde el sistema GTLLite
 			agregarBoton(getBtnAgregarSubPiezas());
 			agregarBoton(getBtnCombinarPiezas());
 			agregarBoton(getBtnDescombinarPiezas());
-			//			getBotonAgregar().setVisible(false);
+			getBotonAgregar().setVisible(false);
 			this.remitoSalida = remitoSalida;
 			if(remitoSalida.getPiezas().isEmpty()) {
 				addRowsInTabla(CANT_PIEZAS_INICIALES, false);
